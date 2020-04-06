8705f9293736b8125544a0aec13a5ab11d946411
Jasper Ho <jasperchho@gmail.com>
2020-04-06 03:24:58 -0400

    v1

--- app.R
+++ app.R
@@ -1,41 +1,131 @@
 library(shiny)
 library (DT)
 library(tidyverse)
+library(readr)
+
+#Data Processing (done once)
+rawtable <- read_csv("https://raw.githubusercontent.com/jzpero/covid19lit/master/data/2020-04-05-21-21-18.csv")
+headers <- colnames(rawtable)
+included.headers <- c("Title", "Author", "Journal","Date",  "PMID", "Type of Study")
+filtered.table <<- rawtable[!duplicated(rawtable$PMID),included.headers]
+unique.authors <<- lapply(strsplit(paste(filtered.table$Author, collapse=','), ","), trimws)[[1]]
+unique.authors <<- sort(unique(unique.authors[lapply(unique.authors, nchar) > 0]))
+unique.journals <<- sort(unique(filtered.table$Journal))
+unique.studytypes <<- sort(unique(filtered.table$`Type of Study`))
+
+createLink <- function(PMID, text) {
+    sprintf('<a href="https://www.ncbi.nlm.nih.gov/pubmed/%s">%s</a>', PMID, text)
+}
 
 ui <- navbarPage(
     title = 'COVID-19 Literature',
     position = c("fixed-top"),
-    tabPanel('All',     DT::dataTableOutput('ex1'),  tags$style(type="text/css", "body {padding-top: 70px;}")),
-    tabPanel('Case Reports',        DT::dataTableOutput('ex2')),
-    tabPanel('Systematic Reviews',        DT::dataTableOutput('ex3')),
-    tabPanel('Meta-analyses',        DT::dataTableOutput('ex4'))
+    tabPanel('Browse',
+         tags$style(type="text/css","body {padding-top: 70px;}"),
+         shinyjs::useShinyjs(),
+         id="tab-panel",
+         sidebarLayout(
+             position = "right",
+             sidebarPanel(
+                 # radioButtons(inputId = "searchtype", "Search behaviour", choices = c("Match ALL criteria (AND)", "Match AT LEAST ONE criteria (OR)")),
+                 selectInput(inputId = "journal", "Journal", choices = NULL, multiple = TRUE),
+                 selectInput(inputId = "study", "Type of Study", choices = NULL, multiple = TRUE),
+                 selectInput(inputId = "author", "Author(s)",choices = NULL, multiple = TRUE),
+                 actionButton(inputId = "clearAll", "Clear All"),
+                 downloadButton(outputId = "export", label = "Download all"),
+                 downloadButton(outputId = "exportselected", label = "Download selected")
+             ),
+             mainPanel(
+                 "Last updated: April 6, 2020. Contains 246 references screened from 1890 results.",
+                 DT::dataTableOutput('ex1')
+             )
+         )
+    ),
+    tabPanel('About',
+         tags$style(type="text/css","body {padding-top: 70px;}"),
+         fluidPage(
+             h1("What is this?"),
+             "Recent literature on COVID-19 is highly variable in scope, quality, and applicability to the front-line physician.",
+             "In a time where information and time are valuable resources, there must be a resource that provides curated primary and secondary literature so that clinical decisions can be made with more confidence.",
+             "This project provides a way to browse current and relevant literature on COVID-19 that has been curated via a systematic review approach.",
+             h1("Contributors"),
+             "This page was coded and designed by Jasper Ho in R using Shiny.", br(),
+             "Citations and data were reviewed and curated by Becky Jones, Daniel Levin, Hannah Kearney, Jasper Ho, and Meghan Glibbery, all medical students at McMaster University.", br(),
+             "Project was conceptualized and managed by Dr. Mark Crowther."
+         )
+    ),
+    collapsible = TRUE
 )
 
-#process the DistillerSR output CSV
-dtable <- read.csv("~/data/2020-04-05-21-21-18.csv", stringsAsFactors=FALSE)
-headers <- colnames(dtable)
-# View(dtable)
-included.headers <- c("Title", "Author", "Journal","Date",  "PMID", "Type.of.Study")
-filtered.table <- dtable[!duplicated(dtable$PMID),included.headers]
+server <- function(input, output, session) {
+    #Populate the search filters
+    updateSelectInput(session, "journal", choices = c("Select a journal" = "", unique.journals))
+    updateSelectInput(session, "author", choices = c("Select authors" = "", unique.authors))
+    updateSelectInput(session, "study", choices = c("Select a type" = "", unique.studytypes))
+    
+    #Initiate table
+    display.table <- filtered.table
+    
+    #clear all button actions
+    observeEvent(input$clearAll, {
+        reset("journal")
+        reset("study")
+        reset("author")
+        cat("Clearing...\n")
+    })
 
-included <- function(input, output) {
-    #All
-    output$ex1 <- DT::renderDataTable(
-        DT::datatable(filtered.table, options = list(pageLength = 25), rownames= FALSE)
+    #Main Data Output
+    output$ex1 <- DT::renderDataTable({
+        #Filter Journal
+        if (!is.null(input$journal)) {
+            display.table <- display.table[display.table$Journal %in% input$journal,]
+        }
+        
+        #Filter Study
+        if (!is.null(input$study)) {
+            display.table <- display.table[display.table$`Type of Study` %in% input$study,]
+        }
+        
+        #Filter Authors
+        if (!is.null(input$author)) {
+            #Initialize FALSE Vector
+            v <- vector("logical",length(display.table$Author))
+            #iterate over author rows and flag as TRUE if any of the selected authors present
+            for (i in 1:length(v)) {
+                for (query in input$author) {
+                    if (grepl(query, display.table$Author[i]))v[i] <- TRUE; break
+                }
+            }
+            display.table <- display.table[v,]
+        }
+        
+        #Create Links
+        display.table$Title <- createLink(display.table$PMID, display.table$Title)
+        
+        #Output
+        DT::datatable(display.table, options = list(pageLength = 25), rownames= FALSE, escape=FALSE)
+    })
+    
+    #Export option
+    output$export <- downloadHandler(
+        filename = function() {
+            paste("references",Sys.Date(),".csv", sep = "")
+        },
+        content = function(file) {
+            write.csv(display.table, file, row.names = FALSE)
+        }
     )
-    #Case Reports
-    output$ex2 <- DT::renderDataTable(
-        DT::datatable(filtered.table[grepl("Case", filtered.table$Type.of.Study),], options = list(pageLength = 25), rownames= FALSE)
-    )
-    #Systematic
-    output$ex3 <- DT::renderDataTable(
-        DT::datatable(filtered.table[grepl("Systematic", filtered.table$Type.of.Study),], options = list(pageLength = 25), rownames= FALSE)
-    )
-    #Meta-analysis
-    output$ex4 <- DT::renderDataTable(
-        DT::datatable(filtered.table[grepl("Meta", filtered.table$Type.of.Study),], options = list(pageLength = 25), rownames= FALSE)
+    
+    output$exportselected <- downloadHandler(
+        filename = function() {
+            paste("references",Sys.Date(),".csv", sep = "")
+        },
+        content = function(file) {
+            write.csv(display.table[input$ex1_rows_selected,], file, row.names = FALSE)
+        }
     )
 }
 
 # Run the application 
-shinyApp(ui = ui, server = included)
+shinyApp(ui = ui, server = server)
+
