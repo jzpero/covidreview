2fab4114fa59f04a6f1697267d2cbb69a66b8754
Jasper Ho <jasperchho@gmail.com>
2020-06-12 17:30:08 -0400

    June 12 update

--- server.R
+++ server.R
@@ -123,7 +123,7 @@ server <- function(input, output, session) { # Executes once per session (no nee
     updateSelectizeInput(session, "specialty", choices = unique.specialties, server = T)
   })
   
-  showNotification("Updated June 10 2020.", type="message")
+  showNotification("Updated June 12 2020.", type="message")
 
   #Main Data Output
   output$ex1 <- DT::renderDataTable(server=TRUE, {
